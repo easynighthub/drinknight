e214b4bf9818e1fa00b9c2b49c58efa0812b65c6
andro ostoic <androstoic@gmail.com>
2018-12-28 15:50:55 -0300

    productos en pantalla 2

--- src/app/home-detalle/home-detalle.component.ts
+++ src/app/home-detalle/home-detalle.component.ts
@@ -4,6 +4,19 @@ import { Event } from '../core/event.model';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 
+
+
+export interface Item {
+  desc: string;
+  img: string;
+  name: string;
+  price: number;
+  title: string;
+  uid: string;
+
+
+}
+
 @Component({
   selector: 'app-home-detalle',
   templateUrl: './home-detalle.component.html',
@@ -11,9 +24,9 @@ import { Router } from '@angular/router';
 })
 export class HomeDetalleComponent implements OnInit {
 
-  private eventsCollection: AngularFirestoreCollection<Event>;
+  private eventsCollection: AngularFirestoreCollection<Item>;
 
-  events: Observable<Event[]>;
+  events: Observable<Item[]>;
 
   selectedEvent: Event;
 
@@ -21,15 +34,18 @@ export class HomeDetalleComponent implements OnInit {
     private router: Router,
     private bd: AngularFirestore
   ) {
-    this.eventsCollection = bd.collection<Event>('events');
+    this.eventsCollection = bd.collection<Item>('productos');
     console.log(this.eventsCollection);
     this.events = this.eventsCollection.valueChanges();
 
-console.log(bd.collection<Event>('events'));
- /*    bd.collection('events').add({
-      name: 'Los Angeles',
-      state: 'CA',
-      country: 'USA'
+console.log(bd.collection<Event>('productos'));
+
+/*     bd.collection('productos').add({
+      desc: '250 Gr. de carne , con esquistos trozos de tocino y queso cheddar + una bebida de 350 cc. + una porción de papas fritas.',
+      img: 'https://firebasestorage.googleapis.com/v0/b/sociallogin-7cfce.appspot.com/o/ImgProductos%2F41C43A4D-B832-4B23-9251-49719FA7D98A.png?alt=media&token=2931f803-bf7a-42a5-bb48-f95725302ae0',
+      name: 'PROMOCION 1',
+      price: 4500,
+      title : '3 MEET'
   })
   .then(function() {
       console.log('Document successfully written!');
